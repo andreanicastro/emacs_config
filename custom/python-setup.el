f1ea8588212eaeec486e71d4ba18a57e0fd88999
anicastro <a.nicastro15@imperial.ac.uk>
2016-08-26 16:58:36 +0100

    initial python support

---
+++ custom/python-setup.el
@@ -0,0 +1,24 @@
+;;python setup
+
+
+
+(use-package elpy
+  :ensure
+  :commands
+  elpy-enable
+
+  :init
+
+  ;; check if python mode
+  (with-eval-after-load 'python (elpy-enable))
+
+  :config
+  ;;set jedi as backed and enable flymake
+  (progn
+    (setq elpy-rpc-backend "jedi"
+	  elpy-rpc-project-specific 't)
+    (when (fboundp 'flycheck-mode)
+      (setq elpy-modules (delete 'elpy-module-flymake elpy-modules)))))
+
+
+(provide 'python-setup)
