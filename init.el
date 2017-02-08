a07d649c782761728b5a2fcb25df389968fcd43a
Andrea Nicastro <a.nicastro15@imperial.ac.uk>
2017-02-08 13:58:27 +0000

    Adjusted company-idle-delay form 0.2 to 0.1

--- init.el
+++ init.el
@@ -61,7 +61,7 @@
    c++-mode-map ([(tab)] . company-complete))
   :config
   (add-hook 'after-init-hook 'global-company-mode)
-  (setq company-idle-delay 0.2)
+  (setq company-idle-delay 0.1)
 )
 
 
