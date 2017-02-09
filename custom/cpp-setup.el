8eee5edface8d62f62390ad7f23fe296be2cd522
anicastro <a.nicastro15@imperial.ac.uk>
2017-02-09 23:50:16 +0000

    improved rtags

--- custom/cpp-setup.el
+++ custom/cpp-setup.el
@@ -23,9 +23,45 @@
 (use-package rtags
   :ensure t
   :config
-  (setq rtags-completions-enabled t)
-  (setq rtags-autostart-diagnostics t)
+  (setq rtags-completions-enabled t
+	rtags-autostart-diagnostics t
+	rtags-use-helm t)
   (rtags-enable-standard-keybindings)
+
+;; (eval-after-load 'cc-mode
+;;   '(progn
+;;      (require 'rtags)
+;;      (mapc (lambda (x)
+;;              (define-key c-mode-base-map
+;;                (kbd (concat "C-c r " (car x))) (cdr x)))
+;;            '(("." . rtags-find-symbol-at-point)
+;;              ("," . rtags-find-references-at-point)
+;;              ("v" . rtags-find-virtuals-at-point)
+;;              ("V" . rtags-print-enum-value-at-point)
+;;              ("/" . rtags-find-all-references-at-point)
+;;              ("Y" . rtags-cycle-overlays-on-screen)
+;;              (">" . rtags-find-symbol)
+;;              ("<" . rtags-find-references)
+;;              ("-" . rtags-location-stack-back)
+;;              ("+" . rtags-location-stack-forward)
+;;              ("D" . rtags-diagnostics)
+;;              ("G" . rtags-guess-function-at-point)
+;;              ("p" . rtags-set-current-project)
+;;              ("P" . rtags-print-dependencies)
+;;              ("e" . rtags-reparse-file)
+;;              ("E" . rtags-preprocess-file)
+;;              ("R" . rtags-rename-symbol)
+;;              ("M" . rtags-symbol-info)
+;;              ("S" . rtags-display-summary)
+;;              ("O" . rtags-goto-offset)
+;;              (";" . rtags-find-file)
+;;              ("F" . rtags-fixit)
+;;              ("X" . rtags-fix-fixit-at-point)
+;;              ("B" . rtags-show-rtags-buffer)
+;;              ("I" . rtags-imenu)
+;;              ("T" . rtags-taglist)))))
+
+  
 )
 
 ;; rtags based backend for company
@@ -82,7 +118,8 @@
   )
 ;; set up the flycheck interface with rtags
 (use-package  flycheck-rtags
-  :after flycheck 
+  :after flycheck
+  :ensure rtags
   :config 
   (flycheck-select-checker 'rtags)
   (setq-local flycheck-highlighting-mode nil)
