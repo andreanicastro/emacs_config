426f3ecb75fd3c11d88f0bab4b73fe2165b8349e
anicastro <a.nicastro15@imperial.ac.uk>
2017-02-10 17:17:30 +0000

    ORGMODE: added support for paper notes

--- custom/orgmode-setup.el
+++ custom/orgmode-setup.el
@@ -5,6 +5,7 @@
   (define-key global-map "\C-cl" 'org-store-link)
   (define-key global-map "\C-ca" 'org-agenda)
   (define-key global-map "\C-cb" 'org-iswitchb)
+  (define-key global-map "\C-cc" 'org-capture)
   (setq org-log-done t) 
 
 
@@ -25,11 +26,37 @@
 	'(("A" todo "ACTIVE")
 	  ("v" tags-todo "VIN")
 	  ("u" tags-todo "STUDY")))
-  
+
+  ;; org capture templates
+  (setq org-capture-templates
+	'(
+	  ("p"          ; key
+	   "Paper"      ; name
+	   entry        ; type
+	   (file+headline "~/Documents/notes/references/papers.org" "Article") ; target
+	   "* %^{Title} %(org-set-tags)  :article: \n:PROPERTIES:\n:Created: %U\n:Linked: %A\n:END:\n%i\nBrief description:\n%?"  ; template
+	   :prepend t     ; property: add at the end
+	   :empty-lines 1 ; property: one blank line before and after the headline
+	   :created t     ; property
+	   ))
+	)
   )
 
 
+(use-package org-ref
+  :config
+  (setq org-ref-bibliography-notes "~/Documents/notes/references/notes"
+	org-ref-bibliography-notes "~/Documents/notes/references/papers.org"
+	org-ref-default-bibliography '("~/Documents/notes/references/papers.bib")
+	org-ref-pdf-directory "~/Documents/notes/references/pdfs/")
+  )
 
 
+(use-package helm-bibtex
+  :config
+  (setq helm-bibtex-bibliography "~/Documents/notes/references/papers.bib"
+	helm-bibtex-library-path "~/Documents/notes/references/pdfs"
+	helm-bibtex-notes-path "~/Documents/notes/references/papers.org")
+  )
 
 (provide 'orgmode-setup)
